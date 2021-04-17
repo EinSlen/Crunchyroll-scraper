81773d1d7f05a3922079c1b739a28038444c6ddd
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-17 17:41:58 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,9 @@
+function date() {
+    let d = new Date();
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+    return `${ye}-${mo}-${da}`;
+}
+
+module.exports = { date }
