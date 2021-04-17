c569151574f52925647c6122bfe854bfef1a8a89
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-17 17:42:56 +0200

    Create pageScraper.js

---
+++ pageScraper.js
@@ -0,0 +1,33 @@
+const { date } = require('./function')
+const { url } = require('./package.json');
+
+const scraperObject = {
+    url: `${url}${date()}`,
+    async scraper(browser){
+        let page = await browser.newPage();
+        console.log(`Navigating to ${this.url}...`);
+        await page.goto(this.url);
+           // await page.click('body > #template_scroller > #template_container > #template_body > .simulcast-calendar > .calendar > .viewport > .content > ol > li > section > div > ol > li > article');
+            //body > #template_scroller > #template_container > #template_body > .simulcast-calendar > .calendar > .viewport > .content > ol > li > section > div > ol > li > article
+          //  await page.waitFor(1000);
+          
+        await page.waitForSelector('.calendar');
+    
+        const result = await page.evaluate(() => {
+        
+            var a = document.querySelectorAll('.release')
+            
+            for(var i = 0; i < a.length; i++) {
+                let title = document.querySelectorAll('cite')[i].innerContent
+                let time = document.querySelectorAll('.available-time')[i].innerContent
+                let episode = document.querySelectorAll('.available-episode-link')[i].innerContent
+                return { title, time, episode }
+            }
+            return result;
+        })
+        console.log(result);
+        await browser.close();
+    }
+}
+
+module.exports = scraperObject;
